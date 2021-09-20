41302ecbf260cfa9c7833134001ab2f543622304
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2021-09-20 21:26:56 +0530

    lists & tuples

---
+++ python/lists.py
@@ -0,0 +1,15 @@
+l1 = [10, 20, 30, 40, True, "Hello", [0, 0, 0]]
+
+l1[0] = 100
+
+l1.append("Good morning")
+print(l1)
+
+l1.insert(1, "Hello")
+print(l1)
+
+removedItem = l1.pop()
+print(removedItem)
+print(l1)
+
+l2 = []
---
+++ python/paper scissor stone.py
@@ -0,0 +1,25 @@
+win1 = 0
+win2 = 0
+
+while True:
+
+    if win1 == 2 or win2 == 2:
+        break
+
+    p1 = input("First player's choice: ")
+    p2 = input("Second player's choice: ")
+
+    if ():
+        win1 += 1 #win = win + 1
+        print("First player wins")
+    elif ():
+        win2 += 1
+        print("Second player wins")
+    elif p1 == p2:
+        print("Draw")
+
+
+if win1 > win2:
+    print("Player 1 is the final winner")
+elif win1 < win2:
+    print("Player 2 is the final winner")
