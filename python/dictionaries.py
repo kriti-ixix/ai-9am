433e10f4894d20577925b4980bdb2422c89dfef1
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2021-09-22 15:11:45 +0530

    Add files via upload

---
+++ python/dictionaries.py
@@ -0,0 +1,11 @@
+d1 = {"name":"Kriti", "subject":"Python", "marks":45, True:1, False:0}
+print(d1)
+
+print(d1["name"])
+print(d1[True])
+
+d1["marks"] = 30
+print(d1)
+
+d1["gender"] = "F"
+print(d1)
