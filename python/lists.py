4919d306ac0a77b2f4f4593dd13b0be411e5a8ee
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2021-09-23 11:03:51 +0530

    lists using range

--- python/lists.py
+++ python/lists.py
@@ -1,18 +1,18 @@
-# l1 = [10, 20, 30, 40, True, "Hello", [0, 0, 0]]
+l1 = [10, 20, 30, 40, True, "Hello", [0, 0, 0]]
 
-# l1[0] = 100
+l1[0] = 100
 
-# l1.append("Good morning")
-# print(l1)
+l1.append("Good morning")
+print(l1)
 
-# l1.insert(1, "Hello")
-# print(l1)
+l1.insert(1, "Hello")
+print(l1)
 
-# removedItem = l1.pop()
-# print(removedItem)
-# print(l1)
+removedItem = l1.pop()
+print(removedItem)
+print(l1)
 
-# l2 = []
+l2 = []
 
 rangeList = list(range(100))
 print(rangeList)
